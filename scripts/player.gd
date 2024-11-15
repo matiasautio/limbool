1b92aa403e17f169a616c580849246add7414ed2
Tatu Heinämäki <tatu.heinamaki@gmail.com>
2024-11-15 15:20:43 +0200

    New 3rd person controller and test playground level.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,46 +1,79 @@
 extends CharacterBody3D
 
+const LERP_VALUE : float = 0.15
 
-@export var speed = 2.5
-const JUMP_VELOCITY = 4.5
+var snap_vector : Vector3 = Vector3.DOWN
+var speed : float
+
+@export_group("Movement variables")
+@export var walk_speed : float = 2.0
+@export var run_speed : float = 5.0
+@export var jump_strength : float = 15.0
+@export var gravity : float = 50.0
 @export var acceleration = 0.25
 @export var friction = 0.05
 
-@export var current = false
+const ANIMATION_BLEND : float = 7.0
 
-@export var camera : Camera3D
+@onready var player_mesh : Node3D = $Mesh
+@onready var spring_arm_pivot : Node3D = $SpringArmPivot
+@onready var animator : AnimationTree = $AnimationTree
 
-# TODO: Changing directions is too instant to feel like floating
 
-func _ready():
-	Input.add_joy_mapping("030000000d0f0000ab01000095000000,HORIPAD STEAM,a:b0,b:b1,y:b4,x:b3,start:b11,back:b10,leftstick:b16,rightstick:b14,leftshoulder:b6,rightshoulder:b7,dpup:b12,dpleft:b14,dpdown:b13,dpright:b15,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a5,righttrigger:a4,platform:Mac OS X")
+#func _ready():
+	#Input.add_joy_mapping("030000000d0f0000ab01000095000000,HORIPAD STEAM,a:b0,b:b1,y:b4,x:b3,start:b11,back:b10,leftstick:b16,rightstick:b14,leftshoulder:b6,rightshoulder:b7,dpup:b12,dpleft:b14,dpdown:b13,dpright:b15,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a5,righttrigger:a4,platform:Mac OS X")
+
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var move_direction : Vector3 = Vector3.ZERO
+	move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	move_direction.z = Input.get_action_strength("down") - Input.get_action_strength("up")
+	move_direction = move_direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
+	
+	velocity.y -= gravity * delta
+	
+	#if Input.is_action_pressed("run"):
+		#speed = run_speed
+	#else:
+	speed = walk_speed
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if current:
-		direction.z += -0.1
-	if direction:
-		velocity.x = move_toward(velocity.x, direction.x * speed, acceleration * delta)#direction.x * speed / 2
-		velocity.z = move_toward(velocity.z, direction.z * speed, acceleration * delta) #direction.z * speed
-		#if input_dir.x > 0:
-		#	camera.offset.z = -1.5
-		#elif input_dir.x < 0:
-		#	camera.offset.z =  1.5
-		#camera.lerp_speed = 2
+	#velocity.x = move_toward(velocity.x, move_direction.x * speed, acceleration * delta)
+	#velocity.z = move_toward(velocity.z, move_direction.z * speed, acceleration * delta)
+	
+	
+	if move_direction:
+		velocity.x = move_toward(velocity.x, move_direction.x * speed, acceleration * delta)
+		#move_direction.x * speed
+		velocity.z = move_toward(velocity.z, move_direction.z * speed, acceleration * delta)
+		#move_direction.z * speed
+	
+		player_mesh.rotation.y = lerp_angle(player_mesh.rotation.y, atan2(velocity.x, velocity.z), LERP_VALUE)
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction)
 		velocity.z = move_toward(velocity.z, 0, friction)
-		#camera.lerp_speed = 1
-
+	
+	var just_landed := is_on_floor() and snap_vector == Vector3.ZERO
+	var is_jumping := is_on_floor() and Input.is_action_just_pressed("ui_accept")
+	if is_jumping:
+		velocity.y = jump_strength
+		snap_vector = Vector3.ZERO
+	elif just_landed:
+		snap_vector = Vector3.DOWN
+	
+	apply_floor_snap()
 	move_and_slide()
+	animate(delta)
+
+func animate(delta):
+	if is_on_floor():
+		animator.set("parameters/ground_air_transition/transition_request", "grounded")
+		
+		if velocity.length() > 0:
+			if speed == run_speed:
+				animator.set("parameters/iwr_blend/blend_amount", lerp(animator.get("parameters/iwr_blend/blend_amount"), 1.0, delta * ANIMATION_BLEND))
+			else:
+				animator.set("parameters/iwr_blend/blend_amount", lerp(animator.get("parameters/iwr_blend/blend_amount"), 0.0, delta * ANIMATION_BLEND))
+		else:
+			animator.set("parameters/iwr_blend/blend_amount", lerp(animator.get("parameters/iwr_blend/blend_amount"), -1.0, delta * ANIMATION_BLEND))
+	else:
+		animator.set("parameters/ground_air_transition/transition_request", "air")
