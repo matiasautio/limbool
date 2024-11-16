ae383e7c0a8c35a0aba3dd575ad9e8087188e173
tatsoni <tatu.heinamaki@gmail.com>
2024-11-16 23:33:47 +0200

    Added all the items into the repo. Work into level 2.

--- items/item.gd
+++ items/item.gd
@@ -2,11 +2,16 @@ extends Node3D
 
 @export var _name = "key"
 @export var audio : AudioStreamOggVorbis
+@export var textures : Array[CompressedTexture2D]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AudioStreamPlayer.stream = audio
+	if textures.size() > 0:
+		var material = StandardMaterial3D.new()
+		material.albedo_texture = textures.pick_random()
+		$MeshPos.get_child(0).get_child(0).set_surface_override_material(0, material)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
