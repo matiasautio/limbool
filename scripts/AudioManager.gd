0908f5800808089572d9b17bdf66c6df7e7e9126
diandrakathyusha <116161960+diandrakathyusha@users.noreply.github.com>
2024-11-16 14:26:18 +0000

    Added audio manager

---
+++ scripts/AudioManager.gd
@@ -0,0 +1,5 @@
+extends Node
+@onready var donut_impact: AudioStreamPlayer = $DonutImpact
+@onready var trampoline_bounce: AudioStreamPlayer = $TrampolineBounce
+@onready var rubber_duck: AudioStreamPlayer = $RubberDuck
+@onready var ambience_sound: AudioStreamPlayer = $AmbienceSound
