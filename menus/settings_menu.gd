eda2a175bff28d5bac00c15087d79054550681c4
tatsoni <tatu.heinamaki@gmail.com>
2024-11-17 13:54:22 +0200

    Level 3 pieces in place. Fixed the menus and hooked up audio sliders.

--- menus/settings_menu.gd
+++ menus/settings_menu.gd
@@ -5,13 +5,13 @@ func _ready() -> void:
 	#GameManager.fade_in()
 
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	get_tree().change_scene_to_file("res://menus/menu.tscn")
 
 func _on_audio_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/audio_menu.tscn")
+	get_tree().change_scene_to_file("res://menus/audio_menu.tscn")
 
 func _on_sensitivity_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/sensitivity_menu.tscn")
+	get_tree().change_scene_to_file("res://menus/sensitivity_menu.tscn")
 
 func _on_apply_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/settings_menu./.tscn")
+	get_tree().change_scene_to_file("res://menus/settings_menu.tscn")
